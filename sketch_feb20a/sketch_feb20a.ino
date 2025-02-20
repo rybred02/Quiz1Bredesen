eca85e8328eb0ba2b483792b17022dcba151aee4
rybred02 <rcbrede@ilstu.edu>
2025-02-20 14:41:09 -0600

    Beginning of Quiz
    This is the beginning of the quiz. Bredesen

---
+++ sketch_feb20a/sketch_feb20a.ino
@@ -0,0 +1,15 @@
+// Pin assignments
+const int redPin = 4;
+const int greenPin = 3;
+const int bluePin = 2;
+const int buttonRed = 5;
+const int buttonGreen = 6;
+const int buttonBlue = 7;
+
+void setup() {
+
+}
+
+void loop() {
+
+}
