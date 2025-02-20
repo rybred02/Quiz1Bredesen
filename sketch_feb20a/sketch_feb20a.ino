2ef9f4ae5764937231f734336101f0a6d484142b
rybred02 <rcbrede@ilstu.edu>
2025-02-20 14:44:04 -0600

    Sets colors to red blue and green
    The buttons set the colors successfully to red green and blue

--- sketch_feb20a/sketch_feb20a.ino
+++ sketch_feb20a/sketch_feb20a.ino
@@ -6,10 +6,33 @@ const int buttonRed = 5;
 const int buttonGreen = 6;
 const int buttonBlue = 7;
 
-void setup() {
+// Function to set LED color
+void setColor(int red, int green, int blue) {
+    analogWrite(redPin, red);
+    analogWrite(greenPin, green);
+    analogWrite(bluePin, blue);
+}
 
+void setup() {
+    pinMode(redPin, OUTPUT);
+    pinMode(greenPin, OUTPUT);
+    pinMode(bluePin, OUTPUT);
+    pinMode(buttonRed, INPUT);
+    pinMode(buttonGreen, INPUT);
+    pinMode(buttonBlue, INPUT);
 }
 
 void loop() {
+    int redPressed = digitalRead(buttonRed) == HIGH;
+    int greenPressed = digitalRead(buttonGreen) == HIGH;
+    int bluePressed = digitalRead(buttonBlue) == HIGH;
 
+    if (redPressed && !greenPressed && !bluePressed) {
+        setColor(255, 0, 0); // Red
+    } else if (!redPressed && greenPressed && !bluePressed) {
+        setColor(0, 255, 0); // Green
+    } else if (!redPressed && !greenPressed && bluePressed) {
+        setColor(0, 0, 255); // Blue
+        setColor(0, 0, 0); // Off
+    }
 }
